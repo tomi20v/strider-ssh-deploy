df5ffffa3bee29ebc0f4b6838a9a52be9b2288ff
Ilya Radchenko <ilya@burstcreations.com>
2015-02-02 17:02:41 -0500

    Cleanup remote_paths

--- remote_paths.js
+++ remote_paths.js
@@ -1,9 +1,15 @@
+'use strict';
+
+var crypto = require('crypto');
+
 module.exports = function(name) {
-  var remote = '$HOME/'+name;
+  var remote = '$HOME/' + name;
+  var randomId = crypto.randomBytes(16).toString('hex');
+  
   return {
     name: name,
     remote: remote,
-    old: remote+'.old',
-    bundle: "/tmp/package-"+require('crypto').randomBytes(16).toString('hex')+".tar.gz",
-  }
-}
+    old: remote + '.old',
+    bundle: '/tmp/package-' + randomId + '.tar.gz'
+  };
+};
